64898c90ee411b4c4b52277b65180e49e08d6f62
Adel Qalieh <aqalieh95@gmail.com>
2016-11-23 14:21:36 -0500

    Add import script for Reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -55,3 +55,18 @@ csv.each do |row|
 end
 
 puts "There are now #{Visit.count} visits in the database."
+
+# Import reviews
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'reviews.csv'))
+csv = CSV.parse(csv_text, :headers => true)
+csv.each do |row|
+  row_hash = row.to_hash.transform_keys { |key| key.sub(/^rw_/, '').underscore }
+  food_truck_id = row_hash.delete 'ft_id'
+  rv = Review.create! row_hash
+  rv.food_truck_id = food_truck_id
+  # Unescape HTML entities (ex: Chris&#39;s => Chris's)
+  rv.comment = CGI::unescapeHTML(rv.comment)
+  puts "Review ##{rv.id} for #{rv.food_truck.name} saved"
+end
+
+puts "There are now #{Review.count} reviews in the database."

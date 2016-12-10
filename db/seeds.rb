d2bd5939fe227d26bd869c66ce2b6b23e12056b3
Adel Qalieh <aqalieh95@gmail.com>
2016-12-09 21:22:04 -0500

    Rubocopify seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,13 +12,13 @@ require 'csv'
 
 # Import FoodTrucks
 csv_text = File.read(Rails.root.join('lib', 'seeds', 'foodTrucks.csv'))
-csv = CSV.parse(csv_text, :headers => true)
+csv = CSV.parse(csv_text, headers: true)
 csv.each do |row|
-  row_hash = row.to_hash.transform_keys { |key| key.sub(/^ft_/, '')}
+  row_hash = row.to_hash.transform_keys { |key| key.sub(/^ft_/, '') }
   ft = FoodTruck.create! row_hash
   # Unescape HTML entities (ex: Chris&#39;s => Chris's)
-  ft.name = CGI::unescapeHTML(ft.name)
-  ft.blurb = CGI::unescapeHTML(ft.blurb)
+  ft.name = CGI.unescapeHTML(ft.name)
+  ft.blurb = CGI.unescapeHTML(ft.blurb)
   puts "#{ft.name} saved"
 end
 
@@ -26,9 +26,9 @@ puts "There are now #{FoodTruck.count} food trucks"
 
 # Import foods
 csv_text = File.read(Rails.root.join('lib', 'seeds', 'foods.csv'))
-csv = CSV.parse(csv_text, :headers => true)
+csv = CSV.parse(csv_text, headers: true)
 csv.each do |row|
-  row_hash = row.to_hash.transform_keys { |key| key.sub(/^fd_/, '')}
+  row_hash = row.to_hash.transform_keys { |key| key.sub(/^fd_/, '') }
   fd = Food.new
   fd.food_truck_id = row_hash['ft_id']
   fd.type = row_hash['type']
@@ -43,7 +43,7 @@ puts "There are now #{Food.count} foods in the database."
 
 # Import truck visits
 csv_text = File.read(Rails.root.join('lib', 'seeds', 'truckVisits.csv'))
-csv = CSV.parse(csv_text, :headers => true)
+csv = CSV.parse(csv_text, headers: true)
 csv.each do |row|
   vi = Visit.new
   vi.food_truck_id = row['ft_id']
@@ -58,14 +58,14 @@ puts "There are now #{Visit.count} visits in the database."
 
 # Import reviews
 csv_text = File.read(Rails.root.join('lib', 'seeds', 'reviews.csv'))
-csv = CSV.parse(csv_text, :headers => true)
+csv = CSV.parse(csv_text, headers: true)
 csv.each do |row|
   row_hash = row.to_hash.transform_keys { |key| key.sub(/^rw_/, '').underscore }
   food_truck_id = row_hash.delete 'ft_id'
   rv = Review.create! row_hash
   rv.food_truck_id = food_truck_id
   # Unescape HTML entities (ex: Chris&#39;s => Chris's)
-  rv.comment = CGI::unescapeHTML(rv.comment)
+  rv.comment = CGI.unescapeHTML(rv.comment)
   puts "Review ##{rv.id} for #{rv.food_truck.name} saved"
 end
 

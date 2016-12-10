ef259b8dce6d94c8564ed636fd9559cc622648a2
Adel Qalieh <aqalieh95@gmail.com>
2016-12-10 18:19:18 -0500

    Add conversion from weekdays to record attribute

--- app/models/food_truck.rb
+++ app/models/food_truck.rb
@@ -1,2 +1,14 @@
 class FoodTruck < ActiveRecord::Base
+  def self.dow_to_initials(dow)
+    # Convert an integer day of week to attribute name
+    # 0 => Su, 1-5 => MF, 6 => Sa
+    case dow
+    when 0
+      'Su'
+    when 1..5
+      'MF'
+    when 6
+      'Sa'
+    end
+  end
 end
--- test/models/food_truck_test.rb
+++ test/models/food_truck_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class FoodTruckTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'convert weekdays to initials' do
+    assert_equal 'Su', FoodTruck.dow_to_initials(0)
+    (1..5).each do |dow|
+      assert_equal 'MF', FoodTruck.dow_to_initials(dow)
+    end
+    assert_equal 'Sa', FoodTruck.dow_to_initials(6)
+  end
 end

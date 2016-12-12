590be79bb3c61db04f22c17c6fab34c40f9176b6
Adel Qalieh <aqalieh95@gmail.com>
2016-12-11 19:05:11 -0500

    Fix flipped lat/longitude

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,8 @@ class WelcomeController < ApplicationController
   def index
     @food_trucks = FoodTruck.all
     @hash = Gmaps4rails.build_markers(@food_trucks) do |ft, marker|
-      marker.lat ft.long.to_f
-      marker.lng ft.lat.to_f
+      marker.lat ft.lat.to_f
+      marker.lng ft.long.to_f
       marker.infowindow ft.name
     end
     @recent_visits = Visit.order('timestamp DESC').limit(10)
